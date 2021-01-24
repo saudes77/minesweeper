c4946c2f2aede6e2db6dbb4896c07fc5eacf81bd
saudes77 <38159877+saudes77@users.noreply.github.com>
2021-01-24 17:15:33 -0600

    feat: Build out basic layout (#2)

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="border-2 border-indigo-900 rounded-lg p-4">
+      <h1 className="text-2xl text-center font-semibold">
+        Let's play some Minesweeper!
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Tile.jsx
@@ -0,0 +1,7 @@
+const Tile = () => {
+  return (
+    <span className="inline-block border border-pink-900 rounded-md w-8 h-8"></span>
+  );
+}
+
+export default Tile;
