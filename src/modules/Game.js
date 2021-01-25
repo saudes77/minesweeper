139975c13ea63755d17c7c51bf8d531b8a2a998a
Stephanie Sauder <steph_dev@outlook.com>
2021-01-24 18:47:01 -0600

    feat: Create game class w/ board setup logic

---
+++ src/modules/Game.js
@@ -0,0 +1,49 @@
+import Utils from "./Utils";
+
+class Game {
+  constructor({rows, columns, mines}) {
+    this.rowCount = rows
+    this.columnCount = columns
+    this.mineCount = mines
+
+    this.createBoard()
+    this.placeMines()
+  }
+
+  createBoard() {
+    let board = []
+
+    for (let i = 0; i < this.rowCount; i++) {
+      let row = []
+      for (let j = 0; j < this.columnCount; j++) {
+        row.push(0)
+      }
+      board.push(row)
+    }
+
+    this.board = board
+  }
+
+  get isMine(rIndex, cIndex) {
+    return this.board[rIndex][cIndex] === 1
+  }
+
+  placeMine(rIndex, cIndex) {
+    this.board[rIndex][cIndex] = 1
+  }
+
+  placeMines() {
+    let minesPlaced = 0
+
+    while (minesPlaced < this.mineCount) {
+      let rIndex = Utils.getRandomIntInRange(0, this.rowCount - 1)
+      let cIndex = Utils.getRandomIntInRange(0, this.columnCount - 1)
+
+      if (!isMine(rIndex, cIndex)) {
+        placeMine(rIndex, cIndex)
+        minesPlaced++
+      }
+    }
+  }
+}
+export default Game;
